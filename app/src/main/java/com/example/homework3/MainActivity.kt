d46e907d03b3de8a61d52c00e05287158a93bf53
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-07-16 16:44:12 +0300

    Created first page

---
+++ app/src/main/java/com/example/homework3/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.homework3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var editTextName: EditText
+    private lateinit var buttonStartSurvey: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        editTextName = findViewById(R.id.editTextName)
+        buttonStartSurvey = findViewById(R.id.buttonStartSurvey)
+
+        buttonStartSurvey.setOnClickListener {
+            val name = editTextName.text.toString().trim()
+
+            if (name.isNotEmpty()) {
+                val intent = Intent(this@MainActivity, SurveyActivity::class.java)
+                intent.putExtra("name", name)
+                startActivity(intent)
+            }
+        }
+
+    }
+}
